45741a3abec81a2f75daacbfdf342ce8eaf217de
technikerleben <christophvillis@googlemail.com>
2026-08-03 17:24:56 +0200

    Offline-App und APK-Build hinzufügen

---
+++ app/src/main/java/de/wegemann/daenemarkhilfe/MainActivity.java
@@ -0,0 +1,28 @@
+package de.wegemann.daenemarkhilfe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override public void onCreate(Bundle state) {
+        super.onCreate(state);
+        webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+
+    @Override public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
